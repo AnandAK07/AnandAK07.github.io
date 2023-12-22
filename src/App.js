bf14d12b0f946f878ebdf5cb162a5187081961dc
AnandAK07 <anandkumark055@gmail.com>
2023-12-22 20:38:31 +0530

    data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Navbar } from './Components/Navbar';
+import  Home  from './Pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ src/Pages/Projects/ProjectsData.js
@@ -0,0 +1,231 @@
+export const projectsData = [{
+    project_img_dark: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/g63czlj2zdywrbnuaqab",
+    project_img: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/g63czlj2zdywrbnuaqab",
+
+    title: "Glossier",
+    description: "This is a Collaborative Project. We developed this website with a tech stack MongoDB , Express.js, React, Redux, Routes, Node.js, CSS, Firebase. The primary objective of this project is to provide users with a seamless and secure platform for shopping cycle. We had build up all our efforts to do our best in this project. As, the Masai School's Mentorship was specifically to build up our skills and we also accordingly implemented all that teachings in our project to look it at its best.",
+    tech_stack: [{
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        name: "react",
+      },
+      {
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://fashon-website.vercel.app/",
+    github: "https://github.com/AnandAK07/fashon-website",
+  },
+  {
+    project_img_dark: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/shnbboigldljmoc0yynu",
+    project_img: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/shnbboigldljmoc0yynu",
+
+    title: "Gem garden jewelry store",
+    description: "This is a Collaborative Project. We developed this website with a tech stack comprising Mongodb, Express.js, React, Redux, Routes, Node.js, CSS, Firebase. The primary objective of this project is to provide users with a seamless and secure platform for shopping jewelry. We had build up all our efforts to do our best in this project. As, the Masai School's Mentorship was specifically to build up our skills and we also accordingly implemented all that teachings in our project to look it at its best.",
+    tech_stack: [{
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUEuytZnw-WdBb6ZjrN_uS8OzZ6QnLVnMvUw&usqp=CAU",
+        name: "bootstrap",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://gem-garden-jewelry-store.vercel.app/",
+    github: "https://github.com/AnandAK07/gem-garden-jewelry-store",
+  },
+  {
+    project_img: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/mw96k2tbs01iwql71qsb",
+    project_img_dark: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/mw96k2tbs01iwql71qsb",
+
+    title: "BlueFly ",
+    description: "This is clone of E-commerce.com, where we have used HTML, CSS and Javascript to acheive this. In this project we have tried to clone the “BlyFly” website. We had build up all our efforts to do our best in this project. As, the Masai School's Mentorship was specifically to build up our skills and we also accordingly implemented all that teachings in our project to look it at its best.",
+    tech_stack: [{
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://blue-fly-ruddy.vercel.app/",
+    github: "https://github.com/AnandAK07/BlueFly",
+  },
+  {
+    project_img: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/omv6yuyzgvmhwvugygrt",
+    project_img_dark: "https://res.cloudinary.com/ddoccbpq7/image/upload/f_auto,q_auto/omv6yuyzgvmhwvugygrt",
+
+    title: "SyLoan-Finance ",
+    description: "This is a Collaborative Project. We developed this website with a tech stack Mongodb, Express.js, React, Redux,Node.js, CSS, Firebase. The primary objective of this project is to provide users with a seamless and secure platform for managing their financial activities, particularly focusing on loan-related transactions to acheive this. We had build up all our efforts to do our best in this project",
+    tech_stack: [{
+        url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+        name: "html",
+      },
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+        name: "css",
+      },
+      {
+        url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+        name: "js",
+      },
+      {
+        url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+        name: "chakra",
+      },
+      {
+        url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        name: "github",
+      },
+    ],
+    link: "https://sy-loan-finance-jxec.vercel.app/",
+    github: "https://github.com/AnandAK07/SyLoan-Finance/tree/main/syloan_finance",
+  }
+  // {
+  //   project_img: "https://user-images.githubusercontent.com/68837552/260916291-1e1448ea-b678-4cfa-85c8-473fa517d914.png",
+  //   project_img_dark: "https://user-images.githubusercontent.com/68837552/269914781-f50b490f-6f4a-4bf1-b807-74a076f26060.png",
+
+  //   title: "Adventour",
+  //   description: "Adventour is a MERN stack Adventure booking and hotel reservation application. Offering a booking platform for hotel rooms and exploring details about various hotels in various locations. Along with many options for adventures available in area. This was a group project with a team of 4 people. This is a modern and user-friendly travel app built with React, designed to help users plan and organize their travel adventures with ease.",
+  //   tech_stack: [{
+  //       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  //       name: "react",
+  //     },
+  //     {
+  //       url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+  //       name: "tailwind",
+  //     },
+  //     {
+  //       url: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+  //       name: "express"
+  //     },
+  //     {
+  //       url: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+  //       name: "html",
+  //     },
+  //     {
+  //       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png",
+  //       name: "css",
+  //     },
+  //     {
+  //       url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+  //       name: "js",
+  //     },
+  //     {
+  //       url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+  //       name: "github",
+  //     },
+  //   ],
+  //   link: "https://adventour-app.vercel.app/",
+  //   github: "https://github.com/karnking/adventour.github.io",
+  // },
+  // {
+  //   project_img: "https://user-images.githubusercontent.com/68837552/276521570-2f2f0b99-7e7e-4b93-941d-9aedb09c0841.png",
+  //   project_img_dark: "https://user-images.githubusercontent.com/68837552/276520315-5178d85b-1642-42cd-bceb-e6d0665e4ee4.png",
+
+  //   title: "Discord-Clone",
+  //   description: "About Full-stack project offering real-time communication platform with features like instant messaging, attachment sharing, message editing, 1:1 calls, member management, unique invite links, and a sleek UI. It uses Socket.io for real-time communication.",
+  //   tech_stack: [{
+  //       url: "https://www.rlogical.com/wp-content/uploads/2023/03/Rlogical-Blog-Images-thumbnail.webp",
+  //       name: "Nextjs"
+  //     },
+  //     {
+  //       url: "https://images.crunchbase.com/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/a1iakwzbac96qzymzwtq",
+  //       name: "Clerk"
+  //     },
+  //     {
+  //       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  //       name: "react",
+  //     },
+  //     {
+  //       url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+  //       name: "tailwind",
+  //     },
+  //     {
+  //       url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+  //       name: "js",
+  //     },
+  //     {
+  //       url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+  //       name: "github",
+  //     },
+  //   ],
+  //   link: "https://discord-lite.onrender.com/invite/93a9534d-1cb0-4471-bb3e-4954e5c3ca59",
+  //   github: "https://github.com/karnking/discord-clone",
+  // },
+  // {
+  //   project_img: "https://user-images.githubusercontent.com/68837552/279716150-42814da8-dbd2-44b2-935c-ad20c17922e9.png",
+  //   project_img_dark: "https://user-images.githubusercontent.com/68837552/285110254-bd607a38-5d7b-4c95-8459-9e54c80dc650.png",
+
+  //   title: "Masai Flow",
+  //   description: "Revamping Masai School's site with React, we blend original aesthetics with enhanced functionality. Users access course info and live MSAT, with events section showing details and upcoming events. Our rewarding system lets users earn redeemable points for invites or MSAT success.",
+  //   tech_stack: [{
+  //       url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+  //       name: "react",
+  //     },
+  //     {
+  //       url: "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+  //       name: "express"
+  //     },
+  //     {
+  //       url: "https://pbs.twimg.com/profile_images/1452637606559326217/GFz_P-5e_400x400.png",
+  //       name: "MongoDB"
+  //     },
+  //     {
+  //       url: "https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg",
+  //       name: "js",
+  //     },
+  //     {
+  //       url: "https://images.opencollective.com/chakra-ui-pro/61bd1dd/logo/256.png",
+  //       name: "chakra",
+  //     },
+  //     {
+  //       url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+  //       name: "github",
+  //     },
+  //   ],
+  //   link: "https://masai-flow.vercel.app/",
+  //   github: "https://github.com/karnking/Masai-Flow",
+  // },
+];
